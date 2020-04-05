bf6c2cf661456a63f8281643a465966afed733f9
JUCA12 <jdac_06@hotmail.com>
2020-04-05 03:00:06 -0500

    Modifique apps

--- pokeapi.py
+++ pokeapi.py
@@ -11,10 +11,22 @@ class PokeAPI(object):
         uri = f'{self.ENDPOINT}/{x}'
         r = requests.get(uri)
         data = r.json()
+        abilities = data.get('abilities')
 
         return {
             'name': data.get('name'),
             'image': data.get('sprites', {}).get('front_default'),
+            'order': data.get('order'),
+            'height': data.get('height'),
+            'weight': data.get('weight'),
+            'base experience': data.get('base_experience'),
+            'area': data.get('location_area_encounters'),
+            'species': data.get('species',{}).get('url'),
+            'default': data.get('is_default'),
+            'types': data.get('types', {}).get([]),
+            'abilities': data.get('abilities', {}).get([]),
+            'id': data.get('id')
+
         }
 
 
